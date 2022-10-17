88915dc1b12f882c5cb17e1beb0a1125b4720be9
Louis Wright <lw254830@falmouth.ac.uk>
2022-10-17 15:23:21 +0100

    Added Home page with all components + Registration and Login pages

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Pricing } from '@/components/Pricing'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Testimonials } from '@/components/Testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>PopUpTeam - Finding a team made simple for small businesses</title>
+        <meta
+          name="description"
+          content="Finding a team made simple for small businesses"
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Pricing />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
