2cbc393cb44cf2e46355f58b7a10e636314e26dd
Louis Wright <lw254830@falmouth.ac.uk>
2022-10-17 16:31:03 +0100

    added creative dashboard and finished business dashboard

--- client/src/pages/Businessdashboard.jsx
+++ client/src/pages/Businessdashboard.jsx
@@ -16,7 +16,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Dashboard() {
+export default function Businessdashboard() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
