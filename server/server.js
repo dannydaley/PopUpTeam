a38eb1d1f2a001914443427d8be66b387024d7c8
DANNY DALEY <dd252935@falmouth.ac.uk>
2022-10-24 11:34:04 +0100

    Set up multer save loacation to handle file and image uploads

--- server/server.js
+++ server/server.js
@@ -52,6 +52,24 @@ io.on('connection', (socket) => {
 //set up multer middleware for image uploads
 var multer  = require('multer');
 
+// set up storage for file uploads
+const storage = multer.diskStorage({
+  // set destination to public image directory
+  destination: "public/images/uploads",
+  filename: function(req, file, cb){
+    // create a unique suffix so that image names will never have a duplicate
+    //suffix consists of the date, a hyphen and then a large random number 
+       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+     cb(null,"IMAGE-" + uniqueSuffix + ".png");
+     // generate the file name of the file to be added to the public image directory
+     // filename contains path to folder to make things easier across the server
+     // we append .png top the filename so that files are recognised as their format
+     let fileName = "images/uploads/" + "IMAGE-" + uniqueSuffix + ".png" ;
+     // update the string attached to the incoming req.body.image field
+     // this is added to the database as the imageLocation
+     req.body.imageLocations += fileName + ","}
+});
+
 //#region DATABASE SET UP ENDPOINTS
 
 // Json file containing dummy data for easier db setup and testing
