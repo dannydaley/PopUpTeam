0d9e122f30840dc0d7ffc1f85660a6a7f5247ba9
JOSH Haywood <jh248828@falmouth.ac.uk>
2023-01-07 12:46:42 +0000

    Add remote database
    - Add connection details for Heroku SQL hosting

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,19 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
+
+/* If you encounter: 
+  Error: Connection lost: The server closed the connection
+
+  This is heroku closing the connection after inactivity as the free tier is limited to 30 minutes of inactivity
+  To fix this re run npm run dev or npm start in the server folder
+*/
 
 //Database setup
 const db = mysql.createConnection({
-        //Local
-        host: 'localhost',
-        user: 'root',
-        port: 3306,
-        password: 'password',
-        database: 'popup'        
+  host: "eu-cdbr-west-03.cleardb.net",
+  user: "bf0cbc17226879",
+  port: 3306,
+  password: "671885a1",
+  database: "heroku_f29376daf1042d8",
 });
 
 module.exports = db;
