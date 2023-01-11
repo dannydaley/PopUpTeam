0d2cdf38bf19e33be1ab787ae9f73e1b62ce30aa
Danny Daley <dd252935@falmouth.ac.uk>
2023-01-11 18:41:45 +0000

    Got the search working with the alphebetised sorting, still getting duplicate entries on the full directory.

--- client/src/pages/CreativeDirectory.js
+++ client/src/pages/CreativeDirectory.js
@@ -43,6 +43,8 @@ const profile = {
     },
 };
 
+let rawDirectory;
+
 let directory = {
     A: [
     ],
@@ -96,6 +98,59 @@ let directory = {
     ],
 };
 
+let searchResults = {
+    A: [
+    ],
+    B: [       
+    ],
+    C: [
+    ],
+    D: [
+    ],
+    E: [
+    ],
+    F: [
+    ],
+    G: [
+    ],
+    H: [
+    ],
+    I: [
+    ],
+    J: [
+    ],
+    K: [
+    ],
+    L: [
+    ],    
+    M: [
+    ],
+    N: [
+    ],
+    O: [
+    ],
+    P: [
+    ],
+    Q: [
+    ],
+    R: [
+    ],
+    S: [
+    ],
+    T: [
+    ],
+    U: [
+    ],
+    V: [
+    ],
+    W: [
+    ],
+    X: [
+    ],
+    Y: [
+    ],
+}
+
 const team = [
     {
         name: 'Leslie Alexander',
@@ -135,6 +190,7 @@ export default function CreativeDirectory(props) {
     const [sidebarOpen, setSidebarOpen] = useState(false);
     const [renderMessage, setRenderMessage] = useState(false);
     const [directoryLoaded, setDirectoryLoaded] = useState(false);
+    const [directoryList, changeDirectoryList] = useState('directory');
 
     //Replace this with currently logged username string
     const username = "John Smith";
@@ -145,14 +201,16 @@ export default function CreativeDirectory(props) {
     };    
 
 
+
 function getDirectory(){
     axios.get('http://localhost:8080/getDirectory')
         .then(res => {   
+            rawDirectory = res.data;
             // loop through each of the fetched user elements
             let sortedUsers = [];
-            res.data.forEach(element => {                
+            rawDirectory.forEach(element => {                
                 if (!sortedUsers.includes(element.user_name)) {                
-                    // loop through each of the directory keys ie A, B, C etc
+                //     // loop through each of the directory keys ie A, B, C etc
                     sortedUsers.push(element.user_name)
                     Object.keys(directory).forEach(letter => {
                         // if first letter of users last name, raised to upper case matches the directory key
@@ -169,6 +227,86 @@ function getDirectory(){
     }).catch(err => {
         console.log(err);
     });
+}
+    
+function searchDirectory(query) {
+    // only search if query is greater than 3 characters
+    if (query.length > 3) {
+        // empty the search results list
+        searchResults = {
+A: [
+],
+B: [       
+],
+C: [
+],
+D: [
+],
+E: [
+],
+F: [
+],
+G: [
+],
+H: [
+],
+I: [
+],
+J: [
+],
+K: [
+],
+L: [
+],    
+M: [
+],
+N: [
+],
+O: [
+],
+P: [
+],
+Q: [
+],
+R: [
+],
+S: [
+],
+T: [
+],
+U: [
+],
+V: [
+],
+W: [
+],
+X: [
+],
+Y: [
+],
+        }
+        // loop through each user in the raw directory list
+        rawDirectory.forEach(element => {
+            // if the first and last name when joined together (raised to uppercase) contains the query (raised to uppercase)..
+            if ((element.first_name + " " + element.last_name).toLocaleUpperCase().includes(query.toUpperCase())) {                
+                // loop through each of the directory keys ie A, B, C etc
+                Object.keys(searchResults).forEach(letter => {
+                    // if first letter of users last name, raised to upper case matches the directory key
+                    if (element.last_name[0].toUpperCase() === letter) {
+                        // add the element to that directory key
+                        searchResults[letter].push(element)
+                    }
+                });
+            } else {
+                return;
+            }    
+        });
+        // change the state to trigger search results being displayed
+        changeDirectoryList('searchResults')
+    } else {
+        // any less than 3 characters in query, show full directory
+        changeDirectoryList('directory')
+    }
 }
     
 getDirectory();
@@ -422,6 +560,7 @@ getDirectory();
                                                 id="search"
                                                 className="block w-full rounded-md border-gray-300 pl-10 focus:border-blue-600 focus:ring-blue-600 sm:text-sm"
                                                 placeholder="Search"
+                                                onChange={event => searchDirectory(event.target.value)}
                                             />
                                         </div>
                                     </div>
@@ -437,33 +576,62 @@ getDirectory();
                             {/* Directory list */}
                           
                             {directoryLoaded ?
-                                <nav className="min-h-0 flex-1 overflow-y-auto" aria-label="Directory">
-                                    {Object.keys(directory).map((letter) => (
-                                        <div key={letter} className="relative">
-                                            <div className="sticky top-0 z-10 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
-                                                <h3>{letter}</h3>
+                                <nav className="min-h-0 flex-1 overflow-y-auto" aria-label="Directory">                                    
+                                    {directoryList === 'directory' ?
+                                        Object.keys(directory).map((letter) => (
+                                            <div key={letter} className="relative">
+                                                <div className="sticky top-0 z-10 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
+                                                    <h3>{letter}</h3>
+                                                </div>
+                                                <ul role="list" className="relative z-0 divide-y divide-gray-200">
+                                                    {directory[letter].map((person) => (
+                                                        <li key={person.id}>
+                                                            <div className="relative flex items-center space-x-3 px-6 py-5 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-600 hover:bg-gray-50">
+                                                                <div className="flex-shrink-0">
+                                                                    <img className="h-10 w-10 rounded-full" src={"http://localhost:8080/public/" + person.profile_picture} alt="" />
+                                                                </div>
+                                                                <div className="min-w-0 flex-1">
+                                                                    <a href="#" className="focus:outline-none">
+                                                                        {/* Extend touch target to entire panel */}
+                                                                        <span className="absolute inset-0" aria-hidden="true" />
+                                                                        <p className="text-sm font-medium text-gray-900">{person.first_name + " " + person.last_name}</p>
+                                                                        <p className="truncate text-sm text-gray-500">{person.work}</p>
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    ))}
+                                                </ul>
                                             </div>
-                                            <ul role="list" className="relative z-0 divide-y divide-gray-200">
-                                                {directory[letter].map((person) => (
-                                                    <li key={person.id}>
-                                                        <div className="relative flex items-center space-x-3 px-6 py-5 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-600 hover:bg-gray-50">
-                                                            <div className="flex-shrink-0">
-                                                                <img className="h-10 w-10 rounded-full" src={"http://localhost:8080/public/" + person.profile_picture} alt="" />
+                                        ))
+                                        :
+                                        Object.keys(searchResults).map((letter) => (
+                                            <div key={letter} className="relative">
+                                                <div className="sticky top-0 z-10 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
+                                                    <h3>{letter}</h3>
+                                                </div>
+                                                <ul role="list" className="relative z-0 divide-y divide-gray-200">
+                                                    {searchResults[letter].map((person) => (
+                                                        <li key={person.id}>
+                                                            <div className="relative flex items-center space-x-3 px-6 py-5 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-600 hover:bg-gray-50">
+                                                                <div className="flex-shrink-0">
+                                                                    <img className="h-10 w-10 rounded-full" src={"http://localhost:8080/public/" + person.profile_picture} alt="" />
+                                                                </div>
+                                                                <div className="min-w-0 flex-1">
+                                                                    <a href="#" className="focus:outline-none">
+                                                                        {/* Extend touch target to entire panel */}
+                                                                        <span className="absolute inset-0" aria-hidden="true" />
+                                                                        <p className="text-sm font-medium text-gray-900">{person.first_name + " " + person.last_name}</p>
+                                                                        <p className="truncate text-sm text-gray-500">{person.work}</p>
+                                                                    </a>
+                                                                </div>
                                                             </div>
-                                                            <div className="min-w-0 flex-1">
-                                                                <a href="#" className="focus:outline-none">
-                                                                    {/* Extend touch target to entire panel */}
-                                                                    <span className="absolute inset-0" aria-hidden="true" />
-                                                                    <p className="text-sm font-medium text-gray-900">{person.first_name + " " + person.last_name}</p>
-                                                                    <p className="truncate text-sm text-gray-500">{person.work}</p>
-                                                                </a>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                    ))}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        ))                                    
+                                    }
                                 </nav>
                                     
                                 : <></>}
