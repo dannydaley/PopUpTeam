98209582d43bb688791fd2c399299da734c6181b
Josh Haywood <jh248828@falmouth.ac.uk>
2023-01-12 14:20:21 +0000

    Update Header.js
    - Added account icon if logged in to mobile and desktop navigation menu
    - Improved formatting and added commenting to Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,128 +1,226 @@
-import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import { Popover, Transition } from '@headlessui/react';
-import clsx from 'clsx';
+import { Fragment, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import Button from './Button';
-import Container from './Container';
-import Logo from './Logo';
-import NavLink from './index/NavLink';
+import { Popover, Transition } from "@headlessui/react";
+import clsx from "clsx";
+
+import Button from "./Button";
+import Container from "./Container";
+import Logo from "./Logo";
+import NavLink from "./index/NavLink";
 
 function MobileNavLink({ href, children }) {
-    return (
-        <Popover.Button as={Link} href={href} className="block w-full p-2">
-            {children}
-        </Popover.Button>
-    );
-};
+  return (
+    <Popover.Button as={Link} to={href} className="block w-full p-2">
+      {children}
+    </Popover.Button>
+  );
+}
 
 function MobileNavIcon({ open }) {
-    return (
-        <svg
-            aria-hidden="true"
-            className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
-            fill="none"
-            strokeWidth={2}
-            strokeLinecap="round"
-        >
-            <path
-                d="M0 1H14M0 7H14M0 13H14"
-                className={clsx(
-                    'origin-center transition',
-                    open && 'scale-90 opacity-0'
-                )}
-            />
-            <path
-                d="M2 2L12 12M12 2L2 12"
-                className={clsx(
-                    'origin-center transition',
-                    !open && 'scale-90 opacity-0'
-                )}
-            />
-        </svg>
-    );
-};
+  return (
+    <svg
+      aria-hidden="true"
+      className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
+      fill="none"
+      strokeWidth={2}
+      strokeLinecap="round"
+    >
+      <path
+        d="M0 1H14M0 7H14M0 13H14"
+        className={clsx(
+          "origin-center transition",
+          open && "scale-90 opacity-0"
+        )}
+      />
+      <path
+        d="M2 2L12 12M12 2L2 12"
+        className={clsx(
+          "origin-center transition",
+          !open && "scale-90 opacity-0"
+        )}
+      />
+    </svg>
+  );
+}
 
 function MobileNavigation() {
-    return (
-        <Popover>
-            <Popover.Button
-                className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none"
-                aria-label="Toggle Navigation"
-            >
-                {({ open }) => <MobileNavIcon open={open} />}
-            </Popover.Button>
-            <Transition.Root>
-                <Transition.Child
-                    as={Fragment}
-                    enter="duration-150 ease-out"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="duration-150 ease-in"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Get session user data
+    axios.get("http://localhost:8080/auth/signin")
+      .then((res) => {
+        //If user is logged in, set state to true else leave false
+        if (res.data.loggedIn === true) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <Popover>
+      <Popover.Button
+        className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none"
+        aria-label="Toggle Navigation"
+      >
+        {({ open }) => <MobileNavIcon open={open} />}
+      </Popover.Button>
+
+      <Transition.Root>
+        {/* Overlay */}
+        <Transition.Child
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Popover.Overlay className="fixed inset-0 bg-slate-300/50" />
+        </Transition.Child>
+
+        {/* Panel */}
+        <Transition.Child
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            as="div"
+            className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
+          >
+            {/* Navlinks */}
+            <MobileNavLink href="/#features">Features</MobileNavLink>
+            <MobileNavLink href="/#testimonials">Testimonials</MobileNavLink>
+            <MobileNavLink href="/about">About</MobileNavLink>
+            <MobileNavLink href="/become-a-creative">
+              Become a creative
+            </MobileNavLink>
+            <MobileNavLink href="/#pricing">Pricing</MobileNavLink>
+            <hr className="m-2 border-slate-300/40" />
+
+            {isLoggedIn ? (
+              <Link to="/directory">
+                {/* Attribution: https://heroicons.com/ */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6 hover:cursor-pointer"
                 >
-                    <Popover.Overlay className="fixed inset-0 bg-slate-300/50" />
-                </Transition.Child>
-                <Transition.Child
-                    as={Fragment}
-                    enter="duration-150 ease-out"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="duration-100 ease-in"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                >
-                    <Popover.Panel
-                        as="div"
-                        className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
-                    >
-                        <MobileNavLink href="/#features">Features</MobileNavLink>
-                        <MobileNavLink href="/#testimonials">Testimonials</MobileNavLink>
-                        <MobileNavLink href="/about">About</MobileNavLink>
-                        <MobileNavLink href="/become-a-creative">Become a creative</MobileNavLink>
-                        <MobileNavLink href="/#pricing">Pricing</MobileNavLink>
-                        <hr className="m-2 border-slate-300/40" />
-                        <MobileNavLink href="/businesslogin">Sign in</MobileNavLink>
-                    </Popover.Panel>
-                </Transition.Child>
-            </Transition.Root>
-        </Popover>
-    );
-};
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
+              </Link>
+            ) : (
+              <MobileNavLink href="/business-login">
+                Sign in
+              </MobileNavLink>
+            )}
+          </Popover.Panel>
+        </Transition.Child>
+      </Transition.Root>
+    </Popover>
+  );
+}
 
 export default function Header(props) {
-    return (
-        <header className="py-10">
-                <Container>
-                    <nav className="relative z-50 flex justify-between">
-                        <div className="flex items-center md:gap-x-12">
-                            <Link href="/#" aria-label="Home">
-                                <Logo className="h-10 w-auto" />
-                            </Link>
-                            <div className="hidden md:flex md:gap-x-6">
-                                <NavLink href="/#features">Features</NavLink>
-                                <NavLink href="/#testimonials">Testimonials</NavLink>
-                                <NavLink href="/about">About</NavLink>
-                                <NavLink href="/#pricing">Pricing</NavLink>
-                                <NavLink href="/become-a-creative">Become a creative</NavLink>
-                            </div>
-                        </div>
-                        <div className="flex items-center gap-x-5 md:gap-x-8">
-                            <div className="hidden md:block">
-                                <NavLink {...props} href="/business-login">Sign in</NavLink>
-                            </div>
-                            <Button href="/business-register" color="blue">
-                                <span>
-                                    Get started <span className="hidden lg:inline">today</span>
-                                </span>
-                            </Button>
-                            <div className="-mr-1 md:hidden">
-                                <MobileNavigation />
-                            </div>
-                        </div>
-                    </nav>
-                </Container>
-        </header>
-    );
-};
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Get session user data
+    axios.get("http://localhost:8080/auth/signin")
+      .then((res) => {
+        //If user is logged in, set state to true else leave false
+        if (res.data.loggedIn === true) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <header className="py-10">
+      <Container>
+        <nav className="relative z-50 flex justify-between">
+          <div className="flex items-center md:gap-x-12">
+            {/* Logo */}
+            <Link href="/" aria-label="Home">
+              <Logo className="h-10 w-auto" />
+            </Link>
+
+            {/* Navlinks */}
+            <div className="hidden md:flex md:gap-x-6">
+              <NavLink href="/#features">Features</NavLink>
+              <NavLink href="/#testimonials">Testimonials</NavLink>
+              <NavLink href="/about">About</NavLink>
+              <NavLink href="/#pricing">Pricing</NavLink>
+              <NavLink href="/become-a-creative">Become a creative</NavLink>
+            </div>
+          </div>
+
+          {/* Account and promo links */}
+          <div className="flex items-center gap-x-5 md:gap-x-8">
+            <div className="hidden md:block">
+              {isLoggedIn ? (
+                <Link to="/directory">
+                  {/* Attribution: https://heroicons.com/ */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6 hover:stroke-blue-500 hover:cursor-pointer"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
+                </Link>
+              ) : (
+                <NavLink {...props} href="/business-login">
+                  Sign in
+                </NavLink>
+              )}
+            </div>
+
+            <Button href="/business-register" color="blue">
+              <span>
+                Get started <span className="hidden lg:inline">today</span>
+              </span>
+            </Button>
+
+            {/* Mobile menu */}
+            <div className="-mr-1 md:hidden">
+              <MobileNavigation />
+            </div>
+          </div>
+        </nav>
+      </Container>
+    </header>
+  );
+}
