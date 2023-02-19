48467c6305ee66902969a2fc905378e47a28144d
Josh Haywood <jh248828@falmouth.ac.uk>
2023-02-19 01:17:19 +0000

    Refactored auth forms.
    - Replaced Fields with Input component
    - Replaced states with FormData state object
    - Replaced keyPress instances with keyDown
    - Correct error messages
    - General formatting adjustments

--- client/src/pages/BusinessLogin.js
+++ client/src/pages/BusinessLogin.js
@@ -3,18 +3,17 @@ import { useNavigate, Link, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import axios from 'axios';
 
+import useFormData from '../components/authentication/useFormData';
 import AuthLayout from '../components/authentication/AuthLayout';
 import Button from '../components/Button';
-import TextField from '../components/authentication/Fields';
+import Input from '../components/authentication/Input';
 import Logo from '../components/Logo';
 
 export default function Login()  {
   const navigate = useNavigate()
   const location = useLocation();
 
-  const [emailInput, setEmailInput] = useState('');
-  const [passwordInput, setPasswordInput] = useState('');
-
+  const { formData, handleChange } = useFormData();
   const [message, setMessage] = useState('');
 
   useEffect(() => {
@@ -24,16 +23,15 @@ export default function Login()  {
   const validateRow = () => {
     // Validate login
     axios.post('http://localhost:8080/auth/signin', {
-      email: emailInput,
-      password: passwordInput
-    })
-    .then((res) => {
-        setMessage(res.data);
+      email: formData.email,
+      password: formData.password,
+    }).then((res) => {
+      setMessage(res.data);
 
-        //If validation passed
-        if (res.data === 'Login successful') {
-          navigate('/directory')
-        };
+      //If validation passed
+      if (res.data === 'Login successful') {
+        navigate('/directory')
+      };
     });
   };
 
@@ -74,55 +72,39 @@ export default function Login()  {
 
         <form 
           onSubmit={onSubmitSignIn} 
-          onKeyPress={(e) => {
+          onKeyDown={(e) => {
             e.key === 'Enter' && onSubmitSignIn(); //Submit form on enter
           }}
-          className="mt-10 grid z-10 grid-cols-1 gap-y-8"
+          className="mt-10"
         >
           {/* Registered message */}
           {message === "Successfully registered, please login" && (
-            <p class="-mt-4 p-4 rounded font-medium text-xs text-green-700 bg-green-300">Successfully registered, please login</p>
+            <p class="-mt-4 mb-8 p-4 rounded font-medium text-xs text-green-700 bg-green-300">Successfully registered, please login</p>
           )}
 
           {/* Email */}
-          <TextField
-            label="Email address"
-            id="email"
-            name="email"
+          <Input
+            label="Email Address"
             type="email"
-            autoComplete="email"
-            required
-            onChange={(e) => {
-              setEmailInput(e.target.value);
-            }}            
-            class={`${
-              message === 'Email or password are incorrect' ? 'w-full border-red-500 rounded' : 'border-gray-200'
-            }`}
+            name="email"
+            value={formData.email}
+            handleChange={handleChange}
+            error={message === "Email or password are incorrect" ? message : ""}
           />
 
           {/* Password */}
-          <TextField
-            label="Password"
-            id="password"
+          <Input
+            label="Password" 
+            type="password" 
             name="password"
-            type="password"
-            autoComplete="current-password"
-            required 
-            onChange={(e) => {
-              setPasswordInput(e.target.value);
-            }}            
-            class={`${
-              message === 'Incorrect password' ? 'w-full border-red-500 rounded' : message === 'Email or password are incorrect' ? 'w-full border-red-500 rounded' : 'border-gray-200'
-            }`}
+            value={formData.password}
+            handleChange={handleChange}
+            error={message === "Email or password are incorrect" ? message : ""}
           />
 
           {/* Password validation */}
-          {message === 'Incorrect password' && (
-            <p class="col-span-full -mt-4 text-xs italic text-red-500">Incorrect password</p>
-          )}
-
           {message === 'Email or password are incorrect' && (
-            <p class="col-span-full -mt-4 text-xs italic text-red-500">Email or password are incorrect</p>
+            <p class="col-span-full -mt-4 mb-4 text-xs italic text-red-500">Email or password are incorrect</p>
           )}
 
           <div>
