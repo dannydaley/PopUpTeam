3db80f49bf6fb640edfbb3ec79688f65402a5be6
Josh Haywood <jh248828@falmouth.ac.uk>
2023-03-02 16:17:02 +0000

    Resolved dead links in header.
    - Added scrollTo function for Features, Testimonials and Pricing nav links
    - Combined mobile and desktop navlinks into one component
    - General formatting improvements in Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,50 +1,17 @@
-import { Fragment } from "react";
+import { useState, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Popover, Transition } from "@headlessui/react";
-import clsx from "clsx";
 
 import Button from "./Button";
 import Container from "./Container";
 import Logo from "./Logo";
 import NavLink from "./index/NavLink";
-
-function MobileNavLink({ href, children }) {
-    return (
-        <Popover.Button as={Link} href={href} className="block w-full p-2">
-            {children}
-        </Popover.Button>
-    );
-}
-
-function MobileNavIcon({ open }) {
-    return (
-        <svg
-            aria-hidden="true"
-            className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
-            fill="none"
-            strokeWidth={2}
-            strokeLinecap="round"
-        >
-            <path
-                d="M0 1H14M0 7H14M0 13H14"
-                className={clsx(
-                    "origin-center transition",
-                    open && "scale-90 opacity-0"
-                )}
-            />
-            <path
-                d="M2 2L12 12M12 2L2 12"
-                className={clsx(
-                    "origin-center transition",
-                    !open && "scale-90 opacity-0"
-                )}
-            />
-        </svg>
-    );
-}
+import ScrollTo from "../lib/scrollTo";
 
 function MobileNavigation(props) {
     const { profile } = props;
+
+    const [mobileMenu, setMobileMenu] = useState(false);
 
     return (
         <Popover>
@@ -52,9 +19,27 @@ function MobileNavigation(props) {
                 className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none"
                 aria-label="Toggle Navigation"
             >
-                {({ open }) => <MobileNavIcon open={open} />}
+                <svg
+                    aria-hidden="true"
+                    className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
+                    fill="none"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    onClick={() => setMobileMenu(!mobileMenu)}
+                >
+                    {!mobileMenu ? (
+                        <path
+                            d="M0 1H14M0 7H14M0 13H14"
+                        />
+                    ) : (
+                        <path
+                            d="M2 2L12 12M12 2L2 12"
+                        />
+                    )}
+                </svg>
             </Popover.Button>
-            <Transition.Root>
+            
+            <Transition.Root show={mobileMenu}>
                 <Transition.Child
                     as={Fragment}
                     enter="duration-150 ease-out"
@@ -79,24 +64,49 @@ function MobileNavigation(props) {
                         as="div"
                         className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
                     >
-                        <MobileNavLink href="/#features">
-                            Features
-                        </MobileNavLink>
-                        <MobileNavLink href="/#testimonials">
-                            Testimonials
-                        </MobileNavLink>
-                        <MobileNavLink href="/about">About</MobileNavLink>
-                        <MobileNavLink href="/become-a-creative">
+                        <div 
+                            onClick={() => {
+                                ScrollTo({ target: "features",  offset: -80, mobileOffset: -100 });
+                                setMobileMenu(false);
+                            }}
+                        >
+                            <NavLink>
+                                Features
+                            </NavLink>
+                        </div>
+                        <div 
+                            onClick={() => {
+                                ScrollTo({ target: "testimonials",  offset: -80, mobileOffset: -100 });
+                                setMobileMenu(false);
+                            }}
+                        >
+                            <NavLink>
+                                Testimonials
+                            </NavLink>
+                        </div>
+                        <div 
+                            onClick={() => {
+                                ScrollTo({ target: "pricing",  offset: -80, mobileOffset: -100 });
+                                setMobileMenu(false);
+                            }}
+                        >
+                            <NavLink>
+                                Pricing
+                            </NavLink>
+                        </div>
+                        <NavLink href="/about">
+                            About
+                        </NavLink>
+                        <NavLink href="/become-a-creative">
                             Become a creative
-                        </MobileNavLink>
-                        <MobileNavLink href="/#pricing">Pricing</MobileNavLink>
+                        </NavLink>
 
                         {profile.email === "" && (
                             <>
                                 <hr className="m-2 border-slate-300/40" />
-                                <MobileNavLink href="/businesslogin">
+                                <NavLink href="/businesslogin">
                                     Sign in
-                                </MobileNavLink>
+                                </NavLink>
                             </>
                         )}
                     </Popover.Panel>
@@ -104,7 +114,7 @@ function MobileNavigation(props) {
             </Transition.Root>
         </Popover>
     );
-}
+};
 
 export default function Header(props) {
     const { profile, setProfile } = props;
@@ -114,19 +124,31 @@ export default function Header(props) {
             <Container>
                 <nav className="relative z-50 flex justify-between">
                     {/* Navbar */}
-                    <div className="flex items-center md:gap-x-12">
+                    <div className="flex items-center lg:gap-x-12">
                         <Logo 
                             width={90}
                             image={"logo.png"} 
                         />
 
-                        <div className="hidden md:flex md:gap-x-6">
-                            <NavLink href="/#features">Features</NavLink>
-                            <NavLink href="/#testimonials">
-                                Testimonials
+                        <div className="hidden lg:flex lg:gap-x-6">
+                            <div onClick={() => ScrollTo({ target: "features", offset: -80 })}>
+                                <NavLink>
+                                    Features
+                                </NavLink>
+                            </div>
+                            <div onClick={() => ScrollTo({ target: "testimonials", offset: -80 })}>
+                                <NavLink>
+                                    Testimonials
+                                </NavLink>
+                            </div>
+                            <div onClick={() => ScrollTo({ target: "pricing", offset: -80 })}>
+                                <NavLink>
+                                    Pricing
+                                </NavLink>
+                            </div>
+                            <NavLink href="/about">
+                                About
                             </NavLink>
-                            <NavLink href="/about">About</NavLink>
-                            <NavLink href="/#pricing">Pricing</NavLink>
                             <NavLink href="/become-a-creative">
                                 Become a creative
                             </NavLink>
@@ -134,10 +156,10 @@ export default function Header(props) {
                     </div>
 
                     {/* Sign in and register */}
-                    <div className="flex items-center gap-x-5 md:gap-x-8">
+                    <div className="flex items-center gap-x-5 lg:gap-x-8">
                         {profile.email === "" ? (
                             <>
-                                <div className="hidden md:block">
+                                <div className="hidden lg:block">
                                     <NavLink {...props} href="/business-login">
                                         Sign in
                                     </NavLink>
@@ -175,7 +197,7 @@ export default function Header(props) {
                             </Link>
                         )}
 
-                        <div className="-mr-1 md:hidden">
+                        <div className="-mr-1 lg:hidden">
                             <MobileNavigation
                                 profile={profile}
                                 setProfile={setProfile}
@@ -186,4 +208,4 @@ export default function Header(props) {
             </Container>
         </header>
     );
-}
+};
--- client/src/components/index/NavLink.js
+++ client/src/components/index/NavLink.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
+import { Popover } from '@headlessui/react';
 
 export default function NavLink({ href, children }) {
-    return (
-        <Link
-            to={href}
-            className="inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
-        >
-            {children}
-        </Link>
-    );
+  return (
+    <Link
+      to={href}
+      as={Popover.Button}
+      className="inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
+    >
+      {children}
+    </Link>
+  );
 };
