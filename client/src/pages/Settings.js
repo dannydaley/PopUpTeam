42cc2766a786e6661efb61a10410a4fab82d7246
Josh Haywood <jh248828@falmouth.ac.uk>
2023-03-14 16:38:34 +0000

    Refactor settings page.
    - Removed unused code from settings page

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -20,7 +20,6 @@ export default function Settingspage(props) {
         axios
             .get(process.env.REACT_APP_SERVER + "/auth/signin")
             .then((res) => {
-                //If user is logged in set login data
                 if (res.data.loggedIn === true) {
                     setUsername(res.data.username);
                     setUsername(res.data.username);
@@ -54,7 +53,6 @@ export default function Settingspage(props) {
                 }
             )
             .then((res) => {
-                // this.props.updateProfilePicture(profilePicture);
                 this.setState({ profilePicture: res.data.profilePicture });
             });
     };
@@ -79,12 +77,6 @@ export default function Settingspage(props) {
         <div class="flex">
             <SideBar
                 account={account}
-                //Replace with actual user data from user session
-                userData={{
-                    firstName: "test",
-                    lastName: "test",
-                    Picture: "test",
-                }}
             />
 
             <form className="space-y-6 px-6 py-6" action="#" method="POST">
@@ -97,29 +89,6 @@ export default function Settingspage(props) {
                             </p>
                         </div>
                         <div className="mt-5 space-y-6 md:col-span-2 md:mt-0">
-                            <div className="grid grid-cols-3 gap-6">
-                                <div className="col-span-3 sm:col-span-2">
-                                    <label
-                                        htmlFor="company-website"
-                                        className="block text-sm font-medium text-gray-700"
-                                    >
-                                        Website
-                                    </label>
-                                    <div className="mt-1 flex rounded-md shadow-sm">
-                                        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-                                            http://
-                                        </span>
-                                        <input
-                                            type="text"
-                                            name="company-website"
-                                            id="company-website"
-                                            className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                            placeholder="www.example.com"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-
                             <div>
                                 <label
                                     htmlFor="about"
@@ -140,11 +109,23 @@ export default function Settingspage(props) {
                                         }
                                     />
                                 </div>
-                                <p className="mt-2 text-sm text-gray-500">
-                                    Brief description for your . URLs are
-                                    hyperlinked.
-                                </p>
                             </div>
+
+                            <div className="w-1/2 col-span-6 sm:col-span-3">
+                                    <label
+                                        htmlFor="work"
+                                        className="block text-sm font-medium text-gray-700"
+                                    >
+                                        Work
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="work"
+                                        id="work"
+                                        autoComplete="family-name"
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                    />
+                                </div>
 
                             <div>
                                 <label className="block text-sm font-medium text-gray-700">
@@ -315,7 +296,6 @@ export default function Settingspage(props) {
                                         name="country"
                                         autoComplete="country-name"
                                         className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                        // onChange={(event) => setNewCountry(event.target.value)}
                                     >
                                         <option>Canada</option>
                                         <option>United States</option>
@@ -337,7 +317,6 @@ export default function Settingspage(props) {
                                         id="street-address"
                                         autoComplete="street-address"
                                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                        // onChange={(event) => setNewStreet(event.target.value)}
                                     />
                                 </div>
 
@@ -354,7 +333,6 @@ export default function Settingspage(props) {
                                         id="city"
                                         autoComplete="address-level2"
                                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                        // onChange={(event) => setNewCity(event.target.value)}
                                     />
                                 </div>
 
@@ -390,152 +368,6 @@ export default function Settingspage(props) {
                                     />
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:p-6">
-                    <div className="md:grid md:grid-cols-3 md:gap-6">
-                        <div className="md:col-span-1">
-                            <h3 className="text-lg font-medium leading-6 text-gray-900">
-                                Notifications
-                            </h3>
-                            <p className="mt-1 text-sm text-gray-500">
-                                Decide which communications you'd like to
-                                receive and how.
-                            </p>
-                        </div>
-                        <div className="mt-5 space-y-6 md:col-span-2 md:mt-0">
-                            <fieldset>
-                                <legend className="sr-only">By Email</legend>
-                                <div
-                                    className="text-base font-medium text-gray-900"
-                                    aria-hidden="true"
-                                >
-                                    By Email
-                                </div>
-                                <div className="mt-4 space-y-4">
-                                    <div className="flex items-start">
-                                        <div className="flex h-5 items-center">
-                                            <input
-                                                id="comments"
-                                                name="comments"
-                                                type="checkbox"
-                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                            />
-                                        </div>
-                                        <div className="ml-3 text-sm">
-                                            <label
-                                                htmlFor="comments"
-                                                className="font-medium text-gray-700"
-                                            >
-                                                Comments
-                                            </label>
-                                            <p className="text-gray-500">
-                                                Get notified when someones posts
-                                                a comment on a posting.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="flex items-start">
-                                        <div className="flex h-5 items-center">
-                                            <input
-                                                id="candidates"
-                                                name="candidates"
-                                                type="checkbox"
-                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                            />
-                                        </div>
-                                        <div className="ml-3 text-sm">
-                                            <label
-                                                htmlFor="candidates"
-                                                className="font-medium text-gray-700"
-                                            >
-                                                Candidates
-                                            </label>
-                                            <p className="text-gray-500">
-                                                Get notified when a candidate
-                                                applies for a job.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="flex items-start">
-                                        <div className="flex h-5 items-center">
-                                            <input
-                                                id="offers"
-                                                name="offers"
-                                                type="checkbox"
-                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                            />
-                                        </div>
-                                        <div className="ml-3 text-sm">
-                                            <label
-                                                htmlFor="offers"
-                                                className="font-medium text-gray-700"
-                                            >
-                                                Offers
-                                            </label>
-                                            <p className="text-gray-500">
-                                                Get notified when a candidate
-                                                accepts or rejects an offer.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </fieldset>
-                            <fieldset>
-                                <legend className="contents text-base font-medium text-gray-900">
-                                    Push Notifications
-                                </legend>
-                                <p className="text-sm text-gray-500">
-                                    These are delivered via SMS to your mobile
-                                    phone.
-                                </p>
-                                <div className="mt-4 space-y-4">
-                                    <div className="flex items-center">
-                                        <input
-                                            id="push-everything"
-                                            name="push-notifications"
-                                            type="radio"
-                                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                        />
-                                        <label
-                                            htmlFor="push-everything"
-                                            className="ml-3 block text-sm font-medium text-gray-700"
-                                        >
-                                            Everything
-                                        </label>
-                                    </div>
-                                    <div className="flex items-center">
-                                        <input
-                                            id="push-email"
-                                            name="push-notifications"
-                                            type="radio"
-                                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                        />
-                                        <label
-                                            htmlFor="push-email"
-                                            className="ml-3 block text-sm font-medium text-gray-700"
-                                        >
-                                            Same as email
-                                        </label>
-                                    </div>
-                                    <div className="flex items-center">
-                                        <input
-                                            id="push-nothing"
-                                            name="push-notifications"
-                                            type="radio"
-                                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                        />
-                                        <label
-                                            htmlFor="push-nothing"
-                                            className="ml-3 block text-sm font-medium text-gray-700"
-                                        >
-                                            No push notifications
-                                        </label>
-                                    </div>
-                                </div>
-                            </fieldset>
                         </div>
                     </div>
                 </div>
