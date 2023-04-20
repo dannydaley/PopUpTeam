2d2a412b75a0044cd051a76361cf9cd0b660feb7
Josh Haywood <jh248828@falmouth.ac.uk>
2023-04-20 16:44:34 +0100

    Create ProjectList component.
    - Created and styled component
    - Added basic sort and filtering functionality

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,11 +1,15 @@
+import ProjectList from "../components/directory/ProjectList";
 import SideBar from "../components/directory/SideBar";
 
 export default function Projects(props) {
   const { account } = props;
 
+
   return (
     <div className="flex">
       <SideBar account={account} />
+
+      <ProjectList />
     </div>
   );
 };
