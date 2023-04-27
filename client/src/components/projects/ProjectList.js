dc836f9b39ac237126d6445b411ebf77e7590d38
Danny Daley <dd252935@falmouth.ac.uk>
2023-04-27 17:26:07 +0100

    Got the project lists working for both leader and member roles

--- client/src/components/projects/ProjectList.js
+++ client/src/components/projects/ProjectList.js
@@ -4,165 +4,175 @@ import { Link } from "react-router-dom";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 
 export default function ProjectList(props) {
-  const {
-    isDesktop,
-    showProjects,
-    setShowProjects,
-    projectList,
-    setProjectList,
-    setProject,
-  } = props;
+    const {
+        memberList,
+        leaderList,
+        isDesktop,
+        showProjects,
+        setShowProjects,
+        projectList,
+        setProjectList,
+        setProject,
+    } = props;
 
-  projectList.sort((a, b) => (a.title > b.title ? 1 : -1)); // Sort projects alphabetically
+    leaderList.sort((a, b) => (a.title > b.title ? 1 : -1)); // Sort projects alphabetically
 
-  const [showCreated, setShowCreated] = useState(true);
-  const [showPartOf, setShowPartOf] = useState(false);
+    const [showCreated, setShowCreated] = useState(true);
+    const [showPartOf, setShowPartOf] = useState(true);
 
-  // Filter results based on search input
-  const handleSearch = (e) => {
-    const searchValue = e.target.value.toLowerCase();
+    // Filter results based on search input
+    const handleSearch = (e) => {
+        const searchValue = e.target.value.toLowerCase();
 
-    // If search bar is empty, display all projects
-    if (searchValue.length === 0) {
-      setProjectList(projectList);
-    } else {
-      // Else, filter projects based on search input
-      const filteredProjects = projectList.filter((project) =>
-        project.title.toLowerCase().includes(searchValue)
-      );
-      setProjectList(filteredProjects);
+        // If search bar is empty, display all projects
+        if (searchValue.length === 0) {
+            setProjectList(projectList);
+        } else {
+            // Else, filter projects based on search input
+            const filteredProjects = projectList.filter((project) =>
+                project.title.toLowerCase().includes(searchValue)
+            );
+            setProjectList(filteredProjects);
+        }
     };
-  };
 
-  return (
-    <>
-      {/* Show projects by default on desktop, else allow toggling */}
-      {(isDesktop || showProjects) && (
-        <div>
-          {/* Project List */}
-          <div className="w-96 h-screen flex flex-col border-r border-gray-200">
-            {/* Heading */}
-            <div className="px-6 pt-6 pb-4">
-              <h2 className="text-lg font-medium text-gray-900">Projects</h2>
-              <p className="mt-1 text-sm text-gray-600">
-                Search project your part of or create a new one
-              </p>
-            </div>
+    return (
+        <>
+            {/* Show projects by default on desktop, else allow toggling */}
+            {(isDesktop || showProjects) && (
+                <div>
+                    {/* Project List */}
+                    <div className="w-96 h-screen flex flex-col border-r border-gray-200">
+                        {/* Heading */}
+                        <div className="px-6 pt-6 pb-4">
+                            <h2 className="text-lg font-medium text-gray-900">
+                                Projects
+                            </h2>
+                            <p className="mt-1 text-sm text-gray-600">
+                                Search project your part of or create a new one
+                            </p>
+                        </div>
 
-            <div className="mt-6 mx-5 space-x-2 flex flex-row">
-              {/* Search bar */}
-              <div className="min-w-0 flex-1">
-                <label htmlFor="search" className="sr-only">
-                  Search
-                </label>
-                <div className="relative rounded-md shadow-sm">
-                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 hover:cursor-pointer">
-                    <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
-                  </div>
-                  <input
-                    type="text"
-                    onChange={handleSearch}
-                    className="block w-full rounded-md border-gray-300 pl-10 focus:border-blue-600 focus:ring-blue-600 sm:text-sm"
-                    placeholder="Search"
-                  />
+                        <div className="mt-6 mx-5 space-x-2 flex flex-row">
+                            {/* Search bar */}
+                            <div className="min-w-0 flex-1">
+                                <label htmlFor="search" className="sr-only">
+                                    Search
+                                </label>
+                                <div className="relative rounded-md shadow-sm">
+                                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 hover:cursor-pointer">
+                                        <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        onChange={handleSearch}
+                                        className="block w-full rounded-md border-gray-300 pl-10 focus:border-blue-600 focus:ring-blue-600 sm:text-sm"
+                                        placeholder="Search"
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Create new project button */}
+                            <Link to="/create-project">
+                                <button className="rounded-md border border-gray-300 p-2 text-center hover:border-gray-400">
+                                    <svg
+                                        className="mx-auto h-6 w-6 text-gray-700"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        stroke="currentColor"
+                                        fill="none"
+                                        viewBox="0 0 48 48"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+                                        />
+                                    </svg>
+                                </button>
+                            </Link>
+                        </div>
+
+                        {/* Project Lists */}
+                        <div
+                            onClick={() => setShowCreated(!showCreated)}
+                            className="mt-6 mb-4 mx-5 text-sm hover:cursor-pointer "
+                        >
+                            Created by you
+                        </div>
+
+                        {showCreated && (
+                            <div className="overflow-y-auto">
+                                {leaderList.map((project, index) => (
+                                    <div
+                                        key={index}
+                                        onClick={() => {
+                                            !isDesktop &&
+                                                setShowProjects(false);
+                                            setProject(project);
+                                        }}
+                                        className={`mx-2 p-4 border-t border-gray-200 ${
+                                            index === projectList.length - 1
+                                                ? "border-b"
+                                                : ""
+                                        }`}
+                                    >
+                                        <div className="flex items-center justify-between">
+                                            <div className="text-sm font-medium text-gray-500">
+                                                {project.title}
+                                            </div>
+
+                                            <div className="text-xs font-medium text-gray-300">
+                                                - {project.role}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+
+                        <div
+                            onClick={() => setShowPartOf(!showPartOf)}
+                            className={`py-4 px-5 text-sm border-t hover:cursor-pointer ${
+                                !showPartOf ? "border-b" : ""
+                            }`}
+                        >
+                            Projects your part of
+                        </div>
+
+                        {showPartOf && (
+                            <div className="overflow-y-auto">
+                                {memberList.map((project, index) => (
+                                    <div
+                                        key={index}
+                                        onClick={() => {
+                                            !isDesktop &&
+                                                setShowProjects(false);
+                                            setProject(project);
+                                        }}
+                                        className={`mx-2 p-4 border-t border-gray-200 ${
+                                            index === projectList.length - 1
+                                                ? "border-b"
+                                                : ""
+                                        }`}
+                                    >
+                                        <div className="flex items-center justify-between">
+                                            <div className="text-sm font-medium text-gray-500">
+                                                {project.title}
+                                            </div>
+
+                                            <div className="text-xs font-medium text-gray-300">
+                                                - {project.role}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                 </div>
-              </div>
-
-              {/* Create new project button */}
-              <Link to="/create-project">
-                <button className="rounded-md border border-gray-300 p-2 text-center hover:border-gray-400">
-                  <svg
-                    className="mx-auto h-6 w-6 text-gray-700"
-                    xmlns="http://www.w3.org/2000/svg"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
-                    />
-                  </svg>
-                </button>
-              </Link>
-            </div>
-
-            {/* Project Lists */}
-            <div
-              onClick={() => setShowCreated(!showCreated)}
-              className="mt-6 mb-4 mx-5 text-sm hover:cursor-pointer "
-            >
-              Created by you
-            </div>
-
-            {showCreated && (
-              <div className="overflow-y-auto">
-                {projectList.map((project, index) => (
-                  <div
-                    key={index}
-                    onClick={() => {
-                      !isDesktop && setShowProjects(false);
-                      setProject(project);
-                    }}
-                    className={`mx-2 p-4 border-t border-gray-200 ${
-                      index === projectList.length - 1 ? "border-b" : ""
-                    }`}
-                  >
-                    <div className="flex items-center justify-between">
-                      <div className="text-sm font-medium text-gray-500">
-                        {project.title}
-                      </div>
-
-                      <div className="text-xs font-medium text-gray-300">
-                        - {project.role}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
             )}
-
-            <div
-              onClick={() => setShowPartOf(!showPartOf)}
-              className={`py-4 px-5 text-sm border-t hover:cursor-pointer ${
-                !showPartOf ? "border-b" : ""
-              }`}
-            >
-              Projects your part of
-            </div>
-
-            {showPartOf && (
-              <div className="overflow-y-auto">
-                {projectList.map((project, index) => (
-                  <div
-                    key={index}
-                    onClick={() => {
-                      !isDesktop && setShowProjects(false);
-                      setProject(project);
-                    }}
-                    className={`mx-2 p-4 border-t border-gray-200 ${
-                      index === projectList.length - 1 ? "border-b" : ""
-                    }`}
-                  >
-                    <div className="flex items-center justify-between">
-                      <div className="text-sm font-medium text-gray-500">
-                        {project.title}
-                      </div>
-
-                      <div className="text-xs font-medium text-gray-300">
-                        - {project.role}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
+        </>
+    );
+}
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -6,57 +6,71 @@ import ProjectList from "../components/projects/ProjectList";
 import ProjectOverview from "../components/projects/ProjectOverview";
 
 export default function Projects(props) {
-  const { isDesktop, account } = props;
+    const { isDesktop, account } = props;
 
-  const [showProjects, setShowProjects] = useState(true);
+    const [showProjects, setShowProjects] = useState(true);
 
-  const [projectList, setProjectList] = useState([]);
-  const [project, setProject] = useState({});
+    const [projectList, setProjectList] = useState([]);
+    const [project, setProject] = useState({});
 
-  // Format creation date
-  useEffect(() => {
-    const date = new Date();
-    const formattedDate = date.toLocaleDateString('en-US', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric'
-    }).replace(/\//g, '-');
+    const [leaderList, setLeaderList] = useState([]);
+    const [memberList, setMemberList] = useState([]);
 
-    setProject(prevState => ({
-      ...prevState,
-      createdAt: formattedDate
-    }));
-  }, []);
+    // Format creation date
+    useEffect(() => {
+        const date = new Date();
+        const formattedDate = date
+            .toLocaleDateString("en-US", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+            })
+            .replace(/\//g, "-");
 
-  // Get all created projects
-  useEffect(() => {
-    axios
-      .get(process.env.REACT_APP_SERVER + "/projects/created")
-      .then((res) => {
-        setProjectList(res.data);
-      });
-  }, []);
+        setProject((prevState) => ({
+            ...prevState,
+            createdAt: formattedDate,
+        }));
+    }, []);
 
-  return (
-    <div className="flex flex-row">
-      <SideBar
-        showProjects={showProjects}
-        setShowProjects={setShowProjects}
-        account={account}
-      />
+    // Get all created projects
+    useEffect(() => {
+        axios
+            .get(process.env.REACT_APP_SERVER + "/projects/created")
+            .then((res) => {
+                setLeaderList(res.data.leader);
 
-      <ProjectList
-        isDesktop={isDesktop}
-        showProjects={showProjects}
-        setShowProjects={setShowProjects}
-        projectList={projectList}
-        setProjectList={setProjectList}
-        setProject={setProject}
-      />
+                return res;
+            })
+            .then((res) => {
+                setMemberList(res.data.member);
+                return res;
+            })
+            .then((res) => {
+                setProjectList(res.data);
+            });
+    }, []);
 
-      <ProjectOverview 
-        project={project}
-      />
-    </div>
-  );
-};
+    return (
+        <div className="flex flex-row">
+            <SideBar
+                showProjects={showProjects}
+                setShowProjects={setShowProjects}
+                account={account}
+            />
+
+            <ProjectList
+                memberList={memberList}
+                leaderList={leaderList}
+                isDesktop={isDesktop}
+                showProjects={showProjects}
+                setShowProjects={setShowProjects}
+                projectList={projectList}
+                setProjectList={setProjectList}
+                setProject={setProject}
+            />
+
+            <ProjectOverview project={project} />
+        </div>
+    );
+}
