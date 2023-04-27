c18935dd17db91af7df0c73a58575d96cf64dba5
Josh Haywood <jh248828@falmouth.ac.uk>
2023-04-27 10:54:43 +0100

    Split projects and directory routes.
    - Renamed search routes to directory
    - Moved project routes to project route file
    - Updated any endpoints using "/search"

---
+++ server/routes/projects.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db");
+
+router.post("/getProjects", (req, res, next) => {
+  let username = req.body;
+  db.query(
+    "SELECT * FROM `relationships` LEFT OUTER JOIN ON `Projects`.`title` WHERE ((`relationships`.`user1` = ? OR `relationships`.`user2` = ?) AND `isProject` = ?)",
+    [username, username, true],
+    (err, data) => {
+      if (err) {
+        // respond with error status and message
+        console.log(err);
+        res.status(500).send(err.message);
+        return;
+      }
+      let projectList = [];
+      data.forEach((element) => {
+        if (element.user1 === username) {
+          projectList.push(element.user2);
+        } else {
+          projectList.push(element.user1);
+        }
+        console.log(projectList);
+      });
+
+      // respond with data if no errors
+      res.json({ data: data });
+    }
+  );
+});
+
+module.exports = router;
