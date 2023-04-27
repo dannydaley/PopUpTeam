e5f96b15ba0505fad1210f2b45d83b223de639c1
Danny Daley <dd252935@falmouth.ac.uk>
2023-04-27 10:14:37 +0100

    Added an endpoint for pulling project data

--- server/routes/search.js
+++ server/routes/search.js
@@ -21,4 +21,32 @@ router.get("/getDirectory", (req, res, next) => {
     );
 });
 
+router.post("/getProjects", (req, res, next) => {
+    let username = req.body;
+    db.query(
+        "SELECT * FROM `relationships` LEFT OUTER JOIN ON `Projects`.`title` WHERE ((`relationships`.`user1` = ? OR `relationships`.`user2` = ?) AND `isProject` = ?)",
+        [username, username, true],
+        (err, data) => {
+            if (err) {
+                // respond with error status and message
+                console.log(err);
+                res.status(500).send(err.message);
+                return;
+            }
+            let projectList = [];
+            data.forEach((element) => {
+                if (element.user1 === username) {
+                    projectList.push(element.user2);
+                } else {
+                    projectList.push(element.user1);
+                }
+                console.log(projectList);
+            });
+
+            // respond with data if no errors
+            res.json({ data: data });
+        }
+    );
+});
+
 module.exports = router;
